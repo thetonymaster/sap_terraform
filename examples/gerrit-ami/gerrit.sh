76bc478f6480b455361c88bd9faf0353e50e69bf
Miguel Eduardo Romero Ruiz <miguelromeroruiz@outlook.com>
2018-01-04 14:34:43 -0600

    Gerrit debian commands explanation

--- examples/gerrit-ami/gerrit.sh
+++ examples/gerrit-ami/gerrit.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-wget https://www.gerritcodereview.com/download/gerrit-2.14.2.war
+wget https://www.gerritcodereview.com/download/gerrit-2.14.2.war //download gerrit
 
-sudo apt-get update -y
-sudo apt-get install default-jdk -y
-sudo apt-get install git -y
-java -jar gerrit*.war init --batch --dev -d ~/gerrit_testsite
-git config --file ~/gerrit_testsite/etc/gerrit.config httpd.listenUrl 'http://localhost:8080'
-~/gerrit_testsite/bin/gerrit.sh restart
+sudo yum -y update //update  yum
 
+sudo yum install -y git //install git
+
+git --version //verifying git is installed
+
+sudo yum install -y java-1.8.0-openjdk.x86_64 //install java
+
+sudo /usr/sbin/alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java 
+
+sudo /usr/sbin/alternatives --set javac /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/javac
+
+sudo yum remove -y java-1.7* //remove java 1.7
+java -jar gerrit*.war init --batch --dev -d ~/gerrit_testsite 
+git config --file ~/gerrit_testsite/etc/gerrit.config httpd.listenUrl 'http://localhost:8080' // URL that Gerrit listens to from * to localhost. This change helps prevent outside connections from contacting the instance.
+~/gerrit_testsite/bin/gerrit.sh restart //restarting gerrit with new configuration
 
 
 
